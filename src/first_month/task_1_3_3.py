61d3be248e7c37e3e2937e6312ba573cb0fcacd0
Armine_Hovhannisyan <armine.a.h@gmail.com>
2021-04-20 16:28:23 +0400

    fixed 1_3_3

--- src/first_month/task_1_3_3.py
+++ src/first_month/task_1_3_3.py
@@ -1,7 +1,7 @@
 #Task 1.3.3
 # 1.Write a Python function which returns factorial value of given number n.
 def factorial(n):
-    if n==1:
+    if n==0 or n==1:
         return  1
     else:
         return factorial(n-1) * n
@@ -10,10 +10,16 @@ def factorial(n):
 
 def fibonaci(n):
     if n==1:
-        return  1
+        return  0
     elif n==2:
-        return  2
+        return  1
     else:
         return fibonaci(n-2) + fibonaci(n-1)
+
+print(fibonaci(1))
+print(fibonaci(2))
+print(fibonaci(3))
+print(fibonaci(4))
+print(fibonaci(5))
 
 
