b011076b4fa78bffaf65430f1dc8cb9879abdda7
Armine_Hovhannisyan <armine.a.h@gmail.com>
2021-04-26 20:07:58 +0400

    1.5.2

--- src/first_month/task_1_5_2.py
+++ src/first_month/task_1_5_2.py
@@ -1,27 +1,25 @@
 class Money:
-    amount = 0
-    currency = ''
 
     def __init__(self, amnt, cur):
-        self.amount = amnt
-        self.currency = cur
+        self.__amount = amnt
+        self.__currency = cur
 
 
     def Print(self):
-        print(str(self.amount) + ' ' + self.currency)
+        print(str(self.__amount) + ' ' + self.__currency)
 
     def Sum(self, money):
-        if(self.currency == money.currency):
-           sum = self.amount + money.amount
-           print('Sum is equal ' + str(sum) + ' ' + self.currency)
+        if(self.__currency == money.__currency):
+           self.__amount += money.__amount
+           return  self
         else:
            print('Cant calculate sum of two different currencies. Please implement converter method')
 
     def Sub(self, money):
-        if(self.currency == money.currency):
-            if(self.amount  >= money.amount):
-                sub = self.amount - money.amount
-                print('Sub is equal ' + str(sub) + ' ' + self.currency)
+        if(self.__currency == money.__currency):
+            if(self.__amount  >= money.__amount):
+                self.__amount -= money.__amount
+                return  self
             else:
                 print('First amount should be greater or equal to second')
         else:
@@ -31,15 +29,20 @@ class Money:
 def main():
     m1 = Money(5, 'usd')
     m1.Print()
-    m2 = Money(10, 'usd')
-    m3 = Money(10, 'eur')
+    m2 = Money(11, 'usd')
+    m3 = Money(12, 'eur')
     m2.Print()
     m3.Print()
     m1.Sum(m2)
+    m1.Print()
     m1.Sum(m3)
+    m1.Print()
     m1.Sub(m2)
+    m1.Print()
     m2.Sub(m1)
+    m2.Print()
     m1.Sub(m3)
+    m1.Print()
 
 
 main()
