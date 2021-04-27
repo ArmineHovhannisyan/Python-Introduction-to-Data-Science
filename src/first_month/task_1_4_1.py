0a642620c1787fb8ce48225485f5835bac04d468
Armine_Hovhannisyan <armine.a.h@gmail.com>
2021-04-27 11:38:14 +0400

    1.3.4 - style corrections

--- src/first_month/task_1_4_1.py
+++ src/first_month/task_1_4_1.py
@@ -2,56 +2,76 @@
 
 # 1. Write a python program which returns a given list without duplicates.
 
-l = [1 ,2, 1, 35, 21, 9, 21, 'a']
-print(list(set(l)))
+def remove_duplicates(l):
+    return list(set(l))
+
+
+print(remove_duplicates([1, 2, 1, 35, 21, 9, 21, 'a']))
 
 # 2. Write a python program which returns common elements of 2 lists.
 
-l1 = [5, 10 ,3, 9]
-l2 = [1 ,2, 5, 9, 36]
-l = set(l1) & set(l2)
-print(l)
+
+def get_intersection(l1, l2):
+    return set(l1) & set(l2)
+
+
+l1 = [5, 10, 3, 9]
+l2 = [1, 2, 5, 9, 36]
+print(get_intersection(l1, l2))
 
 # 3.Write a python program which return elements which are in first list but not in second
 
-l1 = [5, 10 ,3, 9]
-l2 = [1 ,2, 5, 9, 36]
-l = set(l1) - set(l2)
-print(l)
+
+def get_difference(l1, l2):
+    return set(l1) - set(l2)
+
+
+l1 = [5, 10, 3, 9]
+l2 = [1, 2, 5, 9, 36]
+
+print(get_difference(l1, l2))
 
 
 # 4.Write a python program which return elements are or in first list, either in second, but not in both
-l1 = [5, 10 ,3, 9]
-l2 = [1 ,2, 5, 9, 36]
-l = set(l1) ^ set(l2)
-print(l)
+def get_sim_difference(l1, l2):
+    return set(l1) ^ set(l2)
+
+
+l1 = [5, 10, 3, 9]
+l2 = [1, 2, 5, 9, 36]
+
+print(get_sim_difference(l1, l2))
 
 
 # 5.Write a python program which return elements which are or in first, either in second, or in both
-l1 = [5, 10 ,3, 9]
-l2 = [1 ,2, 5, 9, 36]
-l = set(l1) | set(l2)
-print(l)
+def get_union(l1, l2):
+    return set(l1) | set(l2)
+
+
+l1 = [5, 10, 3, 9]
+l2 = [1, 2, 5, 9, 36]
+
+print(get_union(l1, l2))
 
 
 # 6.Write  python function which get set and element value, and remove from set element with given value if exist
 
-def removeFromSet(s, value):
+def remove_from_set(s, value):
     if value in s:
        s.remove(value)
     else:
        print(str(value) + ' is not in given set')
     return s
 
-print(removeFromSet({1, 2, 3}, 2))
-print(removeFromSet({1, 2, 3}, 4))
+print(remove_from_set({1, 2, 3}, 2))
+print(remove_from_set({1, 2, 3}, 4))
 
 # 7.Write a python python program, which return list from given set, where each element of list, is equal to cub of set element
 
-def getCubes(s):
+def get_cubes(s):
    cubes = []
    for i in s:
        cubes.append(i ** 3)
    return cubes
 
-print(getCubes({2, 3, 5, 2}))
+print(get_cubes({2, 3, 5, 2}))
--- src/first_month/task_1_5_2.py
+++ src/first_month/task_1_5_2.py
@@ -5,17 +5,17 @@ class Money:
         self.__currency = cur
 
 
-    def Print(self):
+    def print(self):
         print(str(self.__amount) + ' ' + self.__currency)
 
-    def Sum(self, money):
+    def sum(self, money):
         if(self.__currency == money.__currency):
            self.__amount += money.__amount
            return  self
         else:
            print('Cant calculate sum of two different currencies. Please implement converter method')
 
-    def Sub(self, money):
+    def sub(self, money):
         if(self.__currency == money.__currency):
             if(self.__amount  >= money.__amount):
                 self.__amount -= money.__amount
@@ -28,21 +28,21 @@ class Money:
 
 def main():
     m1 = Money(5, 'usd')
-    m1.Print()
+    m1.print()
     m2 = Money(11, 'usd')
     m3 = Money(12, 'eur')
-    m2.Print()
-    m3.Print()
-    m1.Sum(m2)
-    m1.Print()
-    m1.Sum(m3)
-    m1.Print()
-    m1.Sub(m2)
-    m1.Print()
-    m2.Sub(m1)
-    m2.Print()
-    m1.Sub(m3)
-    m1.Print()
+    m2.print()
+    m3.print()
+    m1.sum(m2)
+    m1.print()
+    m1.sum(m3)
+    m1.print()
+    m1.sub(m2)
+    m1.print()
+    m2.sub(m1)
+    m2.print()
+    m1.sub(m3)
+    m1.print()
 
 
 main()
