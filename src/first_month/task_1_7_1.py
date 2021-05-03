c62f541994b50a677dc1c4d566788b819d6a0a91
Armine_Hovhannisyan <armine.a.h@gmail.com>
2021-05-03 18:41:23 +0400

    1.7.1 - half

--- src/first_month/task_1_7_1.py
+++ src/first_month/task_1_7_1.py
@@ -19,9 +19,61 @@ class Student(Person):
         self.middle_score = middle_score
 
     def __repr__(self):
-        return super().__repr__() + ' ' + str(self.university) + ' ' + str(self.faculty) + ' ' + str(self.course) + ', Score: ' + str(self.middle_score)
+        return super().__repr__() + ' - Info: ' + str(self.university) + ' ' + str(self.faculty) + ' ' + str(self.course) + ', Score: ' + str(self.middle_score)
+
+
+class Teacher(Person):
+    def __init__(self, name, surname, age, gender, university, faculty, discipline , experience, salary):
+        super().__init__(name, surname, age, gender)
+        self.university = university
+        self.faculty = faculty
+        self.discipline  = discipline
+        self.experience  = experience
+        self.salary = salary
+
+    def __repr__(self):
+        return super().__repr__() + ' - Info: ' + str(self.university) + ' ' + str(self.faculty) + ' ' + str(self.discipline) + ', Years at university: ' + str(self.experience) + ', Salary: ' + self.salary.print()
+
+
+
+
+### To Do: Import class Money (dont use explicit in this file)
+
+class Money:
+
+    def __init__(self, amnt, cur):
+        self.__amount = amnt
+        self.__currency = cur
+
+
+    def print(self):
+        return str(self.__amount) + ' ' + self.__currency
+
+    def sum(self, money):
+        if self.__currency == money.__currency:
+           self.__amount += money.__amount
+
+        else:
+           print('Cant calculate sum of two different currencies. Please implement converter method')
+
+    def sub(self, money):
+        if self.__currency == money.__currency:
+            if self.__amount >= money.__amount:
+                self.__amount -= money.__amount
+
+            else:
+                print('First amount should be greater or equal to second')
+        else:
+           print('Cant calculate sub of two different currencies. Please implement converter method')
+
+
+#########
+
+
 
 p = Person('Armine', 'Hovhannisyan', 27, 'Female')
 s = Student('Armine', 'Hovhannisyan', 27, 'Female', 'YSU', 'IAM', 'Discrete Math', '16')
+t = Teacher('Gegham', 'Jivanyan', 30, 'Male', 'Beyond Hub', 'Python', 'Data Science', '?', Money(10000000, 'usd'))
 print(p)
 print(s)
+print(t)
