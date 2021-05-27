c16a57927386408f9c570080e788cafb1dbcff4a
Armine_Hovhannisyan <armine.a.h@gmail.com>
2021-05-27 16:10:44 +0400

    task 2.3 half

---
+++ src/second_month/task_2_3.py
@@ -0,0 +1,16 @@
+# task 2.3
+import numpy as np
+
+# 1. Find min and max in NumPy array
+
+m = np.random.rand(5, 4)
+print(m)
+
+min = np.min(m)
+max = np.max(m)
+print('minimum value: ' + str(min))
+print('maximum value: ' + str(max))
+
+
+
+
